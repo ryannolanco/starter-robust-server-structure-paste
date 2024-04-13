a8e70b48af45d062411d3274944f261e8166882c
Ryan <ryannolanco@gmail.com>
2024-04-12 20:25:43 -0600

    RESTful API lesson

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ const app = express();
 
 const pastes = require("./data/pastes-data");
 
-//SHOW COMPLETE LIST OF ALL PASTES
+// built-in middleware that adds a body property to the request (req.body). The req.body request will contain the parsed data—or it will return an empty object ({}) if there was no body to parse, the Content-Type wasn't matched, or an error occurred
+app.use(express.json())
+
 
 
 //SHOW SINGLE PAST USING A PASTEID
@@ -19,10 +21,29 @@ app.use("/pastes/:pasteId", (req, res, next) => {
     next(`Paste id not found: ${pasteId}`);
   }
 });
-
-app.use("/pastes", (req, res) => {
+//SHOW COMPLETE LIST OF ALL PASTES
+app.get("/pastes", (req, res) => {
   res.json({ data: pastes })
 })
+
+// Variable to hold the next ID
+// Because some IDs may already be used, find the largest assigned ID
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
+
+app.post("/pastes", (req, res, next) => {
+  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
+  const newPaste = {
+    id: ++lastPasteId, // Increment last ID, then assign as the current ID
+    name,
+    syntax,
+    exposure,
+    expiration,
+    text,
+    user_id,
+  };
+  pastes.push(newPaste);
+  res.json({ data: newPaste });
+});
 
 // Not found handler
 app.use((request, response, next) => {
