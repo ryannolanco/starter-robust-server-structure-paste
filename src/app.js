10901b22f2b2c4feec8660aba7b1845c66414230
ryannolanco <ryannolanco@gmail.com>
2024-04-16 22:18:33 -0600

    added controller and router files

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+const pastesRouter = require("./pastes/pastes.router");
 // TODO: Follow instructions in the checkpoint to implement ths API.
 
 const pastes = require("./data/pastes-data");
@@ -10,58 +11,11 @@ app.use(express.json())
 
 
 
-//SHOW SINGLE PAST USING A PASTEID
-app.use("/pastes/:pasteId", (req, res, next) => {
-  const { pasteId } = req.params;
-  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
 
-  if (foundPaste) {
-    res.json({ data: foundPaste });
-  } else {
-    next({ status: 404, message: `Paste id not found: ${pasteId}` });
-  }
-});
-//SHOW COMPLETE LIST OF ALL PASTES
-app.get("/pastes", (req, res) => {
-  res.json({ data: pastes })
-})
+///pastes router
+app.use("/pastes", pastesRouter);
 
 
-//MIDDLEWARE FUNCTION TO TEST IF THERE IS A TEXT PROPERTY
-function bodyHasTextProperty(req, res, next) {
-  const { data: { text } = {} } = req.body;
-  if (text) {
-    return next(); // Call `next()` without an error message if the result exists
-  }
-  next({
-    status: 400,
-    message: "A 'text' property is required.",
-  });
-}
-
-// Variable to hold the next ID
-// Because some IDs may already be used, find the largest assigned ID
-let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
-
-app.post(
-  "/pastes",
-  bodyHasTextProperty, // Add validation middleware function
-  (req, res) => {
-    // Route handler no longer has validation code.
-    const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
-    const newPaste = {
-      id: ++lastPasteId, // Increment last id then assign as the current ID
-      name,
-      syntax,
-      exposure,
-      expiration,
-      text,
-      user_id,
-    };
-    pastes.push(newPaste);
-    res.status(201).json({ data: newPaste });
-  }
-);
 
 // Not found handler
 app.use((request, response, next) => {
